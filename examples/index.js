389bb2c58332f1d7aa54768a51b5912c96cd5f98
Satheesh-C208734 <satheeshkumar.gopisetty@iiflcapital.com>
2025-10-14 11:52:16 +0530

    Update index.js
    updating port and host

--- examples/index.js
+++ examples/index.js
@@ -38,8 +38,8 @@ async function main() {
         JSON.stringify(
           new ConnectReq(
             "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIxVks4TEhlRnRvSmp6YWk1RmJlSGNPbDI3ekpGanBScTE2Vmt4eGJBZ0ZjIn0.eyJleHAiOjE3NjE5OTU1NjUsImlhdCI6MTc0NjQ0MzU2NSwianRpIjoiM2I4ODQ4MzQtM2RiMy00NjZhLTkwMjktMjdmZDE1MjRiNDQwIiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5paWZsc2VjdXJpdGllcy5jb20vcmVhbG1zL0lJRkwiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiODYwYWMzYzMtOTY1Ny00Y2VmLThkMGQtMjk0OGQ1YmUwNzNhIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiSUlGTCIsInNpZCI6IjRmMmQ4N2QzLTc3NjUtNDBmMy04YTA4LTQwYjVhMDc0NGM0OCIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovLzEwLjEyNS42OC4xNDQ6ODA4MC8iXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtaWlmbCIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJJSUZMIjp7InJvbGVzIjpbIkdVRVNUX1VTRVIiLCJBQ1RJVkVfVVNFUiJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJvcGVuaWQgZW1haWwgcHJvZmlsZSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJvbXMiOiJPTlQxIiwidWNjIjoiODM0NzQzNTUiLCJzb2xhY2VfZ3JvdXAiOiJTVUJTQ1JJQkVSX0NMSUVOVCIsIm5hbWUiOiJESVBFU0ggQU5BTlQgQkhBTEVLQVIgTkEiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiI4MzQ3NDM1NSIsImdpdmVuX25hbWUiOiJESVBFU0ggQU5BTlQgQkhBTEVLQVIiLCJmYW1pbHlfbmFtZSI6Ik5BIiwiZW1haWwiOiJiaGFsZWthci5kaXBlc2gxNDlAZ21haWwuY29tIn0.Oh4or2UvV3yTppxM3pKGimbMWVrnrwgb93ax1BpxFJTGV8R-19LzQiSSFLmhMvWCfnTBHFtwKJyZy-OsJ_SEy-k_MBRnTW6kUQVi9puU-wFCpprE9gn27kEQAIGV8ZQQe0bg65wX16AP6b8T6scfpaJM_izuRA-aZYSEhKnbxzZj0PamG7-rE0vl4mL1wFeGh6RNcP8ruAw3reafQgRLMS4WDAb2gmKwYRJCYLEezxStMVPZ9NXx_-oAqAblTX3gyK6yG0Bn0wCAThtcvbdYxw0VRP7xMqOS1w_l6BoqbZtRaxgRofT1R-hDj5Xjw5qJdq4wIvkX9erQVe4-2UM1Fg",
-            "mqtts://sandesh.indiainfoline.com",  //ssl://bridge.iiflcapital.com, wss://bridge.iiflcapital.com, mqtts://sandesh.indiainfoline.com
-            9906
+            "mqtts://bridge.iiflcapital.com",
+            8883
           )
         ),
         (res) => {
